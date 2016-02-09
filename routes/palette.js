86d60a08260f33e07e7f9da495d145864747ad67
Shuming Cao <s5cao@eng.ucsd.edu>
2016-02-08 21:37:02 -0800

    app.js

---
+++ routes/palette.js
@@ -0,0 +1,7 @@
+var palettes = require('../palettes.json');
+
+exports.randomPalette = function(req, res) {
+	// get a random palette from the top ones
+	var randomPalette = palettes[Math.floor(palettes.length * Math.random())];
+	res.json(randomPalette['colors']['hex']);
+}
