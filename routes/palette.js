11e936c62b102379d0befe918e5125981954bbb2
Shuming Cao <s5cao@eng.ucsd.edu>
2016-02-08 21:40:36 -0800

    delete js files

--- routes/palette.js
+++
@@ -1,7 +0,0 @@
-var palettes = require('../palettes.json');
-
-exports.randomPalette = function(req, res) {
-	// get a random palette from the top ones
-	var randomPalette = palettes[Math.floor(palettes.length * Math.random())];
-	res.json(randomPalette['colors']['hex']);
-}
