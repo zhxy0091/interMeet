0f93712f30914c9fd8aff8039ac420b915e6d56a
Anton Qiu <qiuchangtong@gmail.com>
2016-02-09 14:31:26 -0800

    home

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,12 @@
 
 exports.view = function(req, res){
 	var code = req.body.meeting.code;
-    console.log(code);
-  	res.render('home', {
-        'code': code
-    });
+    var firstname = req.body.user.firstname;
+    var lastname = req.body.user.lastname;
+    var joinInfo = {
+        'code': code,
+        'lastname': lastname,
+        'firstname': firstname
+    }
+  	res.render('home', joinInfo);
 };
